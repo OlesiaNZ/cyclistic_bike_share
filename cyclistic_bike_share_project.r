a4a7f307e639f05dbb53418216098c27b5a2fec7
Olesia Dolzhenkova <olesinionz@gmail.com>
2024-01-22 21:02:57 +1300

    Put 1 csv database into var and test that it works

--- cyclistic_bike_share_project.r
+++ cyclistic_bike_share_project.r
@@ -2,3 +2,8 @@ install.packages("tidyverse")
 library(tidyverse)
 install.packages("janitor")
 library(janitor)
+
+
+data_frame_jan <- read.csv("Trip_data_01_2023.csv")
+# Test that csv file loaded into var
+view(head(data_frame_jan, 5))
