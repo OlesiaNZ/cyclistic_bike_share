49380f5a3ec3118f1f075edb57e7c4e662b5d516
Olesia Dolzhenkova <olesinionz@gmail.com>
2024-01-28 16:14:20 +1300

    Change time format with Posix again

--- cyclistic_bike_share_project.r
+++ cyclistic_bike_share_project.r
@@ -66,16 +66,16 @@ data_frame_2023 <- data_frame_2023 %>%
   mutate(started_at = as.character(started_at)) %>%
   separate(started_at, into = c("started_date", "started_time"), sep = " ")
 
-# Convert 'started_date' to Date type 
+# Convert 'started_date' to Date type and 'started_time' to POSIXct type
 data_frame_2023$started_date <- as.Date(data_frame_2023$started_date)
-data_frame_2023$started_time <- strptime(data_frame_2023$started_time, format = "%H:%M:%S")
+data_frame_2023$started_time <- as.POSIXct(data_frame_2023$started_time, format = "%H:%M:%S")
 
 # Do the same with ended_at column
 data_frame_2023 <- data_frame_2023 %>%
   mutate(ended_at = as.character(ended_at)) %>%
   separate(ended_at, into = c("ended_date", "ended_time"), sep = " ")
 data_frame_2023$ended_date <- as.Date(data_frame_2023$ended_date)
-data_frame_2023$ended_time <- strptime(data_frame_2023$ended_time, format = "%H:%M:%S")
+data_frame_2023$ended_time <- as.POSIXct(data_frame_2023$ended_time, format = "%H:%M:%S")
 
 # View the resulting data frame
 view(head(data_frame_2023, 50))
